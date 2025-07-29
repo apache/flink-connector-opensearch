f6aa8b0a6704e95ce2c625be6c63ed18a2518de8
Aaron Sun <645541506@qq.com>
2025-07-29 17:08:27 +0800

    Update DefaultRestClientFactory.java
    Allow All hostname when ssl set insecure

--- flink-connector-opensearch-base/src/main/java/org/apache/flink/connector/opensearch/sink/DefaultRestClientFactory.java
+++ flink-connector-opensearch-base/src/main/java/org/apache/flink/connector/opensearch/sink/DefaultRestClientFactory.java
@@ -21,6 +21,7 @@ package org.apache.flink.connector.opensearch.sink;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.TrustAllStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
