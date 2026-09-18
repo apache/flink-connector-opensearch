d7e2c975ce673ffcc5a44a8d4d5fa32866465d15
Andriy Redko <drreta@gmail.com>
2026-09-17 20:26:39 -0400

    [hotfix] [opensearch] Make branch compatible with Flink 1.19/1.20
    Signed-off-by: Andriy Redko <drreta@gmail.com>

--- flink-connector-opensearch/src/test/java/org/apache/flink/connector/opensearch/sink/OpensearchSinkBuilderTest.java
+++ flink-connector-opensearch/src/test/java/org/apache/flink/connector/opensearch/sink/OpensearchSinkBuilderTest.java
@@ -24,10 +24,8 @@ import org.apache.flink.connector.opensearch.sink.BulkResponseInspector.BulkResp
 import org.apache.flink.metrics.MetricGroup;
 import org.apache.flink.metrics.groups.UnregisteredMetricsGroup;
 import org.apache.flink.streaming.runtime.tasks.TestProcessingTimeService;
-import org.apache.flink.util.FlinkRuntimeException;
 import org.apache.flink.util.SimpleUserCodeClassLoader;
 import org.apache.flink.util.TestLoggerExtension;
-import org.apache.flink.util.function.ThrowingRunnable;
 
 import org.apache.http.HttpHost;
 import org.junit.jupiter.api.DynamicTest;
@@ -163,8 +161,8 @@ class OpensearchSinkBuilderTest {
                         TestingSinkWriterMetricGroup.getSinkWriterMetricGroup(
                                 new UnregisteredMetricsGroup()));
 
-        Mockito.when(sinkInitContext.getMailboxExecutor())
-                .thenReturn(new OpensearchSinkBuilderTest.DummyMailboxExecutor());
+        MailboxExecutor executor = Mockito.mock(MailboxExecutor.class);
+        Mockito.when(sinkInitContext.getMailboxExecutor()).thenReturn(executor);
         Mockito.when(sinkInitContext.getProcessingTimeService())
                 .thenReturn(new TestProcessingTimeService());
         Mockito.when(sinkInitContext.getUserCodeClassLoader())
@@ -174,21 +172,6 @@ class OpensearchSinkBuilderTest {
 
         assertThatCode(() -> sink.createWriter(sinkInitContext)).doesNotThrowAnyException();
         assertThat(called).isTrue();
-    }
-
-    private static class DummyMailboxExecutor implements MailboxExecutor {
-        private DummyMailboxExecutor() {}
-
-        public void execute(
-                ThrowingRunnable<? extends Exception> command,
-                String descriptionFormat,
-                Object... descriptionArgs) {}
-
-        public void yield() throws InterruptedException, FlinkRuntimeException {}
-
-        public boolean tryYield() throws FlinkRuntimeException {
-            return false;
-        }
     }
 
     private OpensearchSinkBuilder<Object> createEmptyBuilder() {
